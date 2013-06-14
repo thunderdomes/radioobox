512959fe73dd228ba1a3d5f8a77931daf1e94936
lateph <lateph@gmail.com>
2013-06-14 18:34:17 +0700

    bug fix

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -30,9 +30,9 @@
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
-				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
+				array('label'=>'Radio', 'url'=>array('/radio', 'view'=>'about')),
+				array('label'=>'Countris', 'url'=>array('/countries')),
+				array('label'=>'States', 'url'=>array('/states'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
 		)); ?>
